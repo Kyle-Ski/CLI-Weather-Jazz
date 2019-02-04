c164678aea017c89058bb2a5840f5def439804e3
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-02-04 14:36:35 -0700

    going with dark sky instead of yahoo weather

--- cmds/today.js
+++ cmds/today.js
@@ -1,3 +1,20 @@
-module.exports = (args) => {
-    console.log('Today is... well today, no weather yet')
+const ora = require('ora')
+const getWeather = require('../utils/weather')
+
+module.exports = async (args) => {
+  const spinner = ora().start()
+
+  try {
+    const location = args.location || args.l
+    const weather = await getWeather(location)
+
+    spinner.stop()
+
+    console.log(`Heads up, the current conditions in ${location}:`)
+    console.log(`\t${weather.condition.temp}° ${weather.condition.text}`)
+  } catch (err) {
+    spinner.stop()
+
+    console.error(err)
+  }
 }
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+require('dotenv').config()
 const minimist = require('minimist')
 
 module.exports = () => {
     const args = minimist(process.argv.slice(2))
-    const cmd = args._[0] || 'help'
+    let cmd = args._[0] || 'help'
 
-    switch (cmd){
-        case 'today': 
+    if (args.version || args.v) {
+        cmd = 'version'
+    }
+
+    if (args.help || args.h) {
+        cmd = 'help'
+    }
+
+    switch (cmd) {
+        case 'today':
             require('./cmds/today')(args)
             break
         case 'help':
